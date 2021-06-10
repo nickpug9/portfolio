c27c6a1d2c7e56e91d27aa4c3e961b3c96a37016
Nick Pugliesi <nickpug9@gmail.com>
2021-06-10 11:28:34 -0400

    new deploy

--- src/Nav.js
+++ src/Nav.js
@@ -1,59 +1,82 @@
+// import Navbar from "react-bootstrap/Navbar";
+// import Button from "react-bootstrap/Button";
+
 import React, { useState } from "react";
 import logo from "./logo.svg";
 import { Link } from "react-router-dom"; // Import the Link component
 
 function NewComponent(props) {
-  const [isActive, setActive] = useState("false");
+  // const [isActive, setActive] = useState("false");
 
-  const ToggleClass = () => {
-    setActive(!isActive);
-  };
+  // const ToggleClass = () => {
+  //   setActive(!isActive);
+  // };
   return (
     <div>
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark position-relative">
-        <a className="navbar-brand" href="/">
+      {/* <Navbar bg="dark" variant="dark" expand="lg">
+        <Navbar.Brand href="/">
           <img src={logo} className="img-fluid logo-image" alt="Logo" />
-        </a>
-        <button
-          className="navbar-toggler"
-          onClick={ToggleClass}
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div
-          className={
-            isActive
-              ? "collapse navbar-collapse "
-              : "collapse navbar-collapse show"
-          }
-          id="navbarNav"
-        >
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <Link to="/home" className="item nav-link ">
-                Home
-              </Link>
-            </li>
-            <li className="nav-item active">
-              <Link to="/projects" className="item nav-link">
-                Projects
-              </Link>
-            </li>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Link to="/" className="nav-link">
+              Home
+            </Link>
+            <Link to="/about" className="nav-link">
+              About me
+            </Link>
+            <Link to="/projects" className="nav-link">
+              Projects
+            </Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar> */}
 
-            <li className="nav-item">
-              <Link to="/about" className="item nav-link">
-                About
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </nav>
+      {/* <nav className="navbar navbar-expand-lg navbar-dark bg-dark position-relative">
+          <a className="navbar-brand" href="/">
+            <img src={logo} className="img-fluid logo-image" alt="Logo" />
+          </a>
+          <button
+            className="navbar-toggler"
+            onClick={ToggleClass}
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div
+            className={
+              isActive
+                ? "collapse navbar-collapse "
+                : "collapse navbar-collapse show"
+            }
+            id="navbarNav"
+          >
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <Link to="/home" className="item nav-link ">
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item active">
+                <Link to="/projects" className="item nav-link">
+                  Projects
+                </Link>
+              </li>
+
+              <li className="nav-item">
+                <Link to="/about" className="item nav-link">
+                  About
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </nav> */}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
-import "bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css";
-import $ from "jquery";
-import Popper from "popper.js";
-import "bootstrap/dist/js/bootstrap.bundle.min";
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
